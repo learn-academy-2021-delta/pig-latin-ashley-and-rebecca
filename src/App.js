17a436250257f96c7c4524772b52e1a047e27994
alappie <ashley@specialtyproduce.com>
2021-09-23 16:44:10 -0700

    Back over to Rebecca

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component{
     this.state = {
       // "phrase" is the text entered by the user - right now there are test words hard coded to make the process of testing your code faster and easier
       // ACTION ITEM: when you are ready for your full user experience, delete the test words so phrase is assigned an empty string
-      phrase: "alpha through yummy squeal queen fry",
+      phrase: "",
       // "phraseTranslated" is what the user will see appear on the page as Pig Latin, it starts as the preset message and updates when your user clicks the "submit" button
       phraseTranslated: "This is where your translated sentence will appear."
     }
@@ -20,6 +20,7 @@ class App extends Component{
   myPigLatinCodeHere = () => {
     // the variable "userInput" will contain the text input from the user modified into an array of words
     // no need to change this variable
+
     let userInput = this.state.phrase.split(" ") //array of words: userInput
     console.log("userInput:", userInput)
 
@@ -37,29 +38,34 @@ class App extends Component{
       // i,e: ['alpha', 'through', 'yummy', 'squeal', 'queen', 'fry']
 
       if(vowelsArray.length === 0){ //no vowel special case ex. fry
-        return value = value.slice(indexOf("y")) + value.slice(0, value.indexOf("y")) + "ay";
+         value = value.slice(value.indexOf("y")) + value.slice(0, value.indexOf("y")) + "ay";
       }
-      else if(){
-        return;
-      }
-      
-      //if else: currentWord contains q - have the split start at the first vowel index instead of the 0 index => concat currentWord.slice(indexOf(e vowelsArray[1]) + currentWord.slice(0, indexOf(+"ay")
-        //queen -> vowels array ['u', 'e', 'e']
+         //if else: value contains q - have the split start at the first vowel index instead of the 0 index => concat value.slice(indexOf(e vowelsArray[1]) + value.slice(0, indexOf(+"ay")
+              //queen -> vowels array ['u', 'e', 'e']
         //squeal
+    
+      else if(vowelsArray[0]=== "u" && value [value.indexOf("u")-1] === "q"){
+         value = value.slice(value.indexOf(vowelsArray[1])) + value.slice(0, value.indexOf(vowelsArray[1])) + "ay"
+      }
+        // if else: test: value[0] != a,e, i, o, u? => return concat: value[0] + value.slice(indexOf(vowelsArray[0])) + "ay"
 
-      // if else: test: currentWord[0] != a,e, i, o, u? => return concat: currentWord[0] + currentWord.slice(indexOf(vowelsArray[0])) + "ay"
+        else if (value[0] !== "a" && value[0]!== "e" && value[0]!== "i" && value[0]!== "o" && value[0]!== "u"){
+      value = value.slice(value.indexOf(vowelsArray[0])) + value.slice(0, value.indexOf(vowelsArray[0])) + "ay"
 
-      // else: test: currentWord[0] = a, e, i, o, u? => return: currentWord + :"way" 
+        }
+         // else: test: value[0] = a, e, i, o, u? => return: value + :"way" 
+        else if(value[0] === "a" || value[0]=== "e" || value[0]=== "i" || value[0]=== "o" || value[0]=== "u"){
+          value = value + "way"
+        }
 
-
-
-
-      //First condition consonants, identify the index of the 1st vowel, if the[0] 
-      //else for no vowels
-
+        else{
+          value = "ERROR"
+        }
+      
+    
       // Remember: console.log is your friend :)
 
-  
+  return value
     })
 
 
@@ -77,7 +83,7 @@ class App extends Component{
     // this method restarts the game by setting the original state
     // ACTION ITEM: when you are ready for your full user experience, delete the test words in phrase so that is assigned an empty string
     this.setState({
-      phrase: "alpha through yummy squeal queen fry",
+      phrase: "",
       phraseTranslated: "This is where your translated sentence will appear."
     })
   }
@@ -98,6 +104,7 @@ class App extends Component{
   render() {
     return (
       <>
+      <div className = "background">
         <h1>Pig Latin Translator</h1>
         <img
           src={butcherPig}
@@ -120,7 +127,8 @@ class App extends Component{
         </div>
         <p>{this.state.phraseTranslated}</p>
         <footer>Coded by Ashley and Rebecca</footer>
-      </>
+        </div>
+        </>
     )
   }
 }
